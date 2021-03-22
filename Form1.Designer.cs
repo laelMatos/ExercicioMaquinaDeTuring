f3aac506685d7569eddd724436b73723f3a4c065
Lael Santos de Matos <36204176+laelMatos@users.noreply.github.com>
2021-03-22 19:04:56 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace MaquinaDeTuring
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Val1 = new System.Windows.Forms.TextBox();
+            this.Val2 = new System.Windows.Forms.TextBox();
+            this.Entrada = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.Bt_iniciar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Val1
+            // 
+            this.Val1.Location = new System.Drawing.Point(12, 12);
+            this.Val1.Name = "Val1";
+            this.Val1.Size = new System.Drawing.Size(100, 23);
+            this.Val1.TabIndex = 0;
+            // 
+            // Val2
+            // 
+            this.Val2.Location = new System.Drawing.Point(12, 41);
+            this.Val2.Name = "Val2";
+            this.Val2.Size = new System.Drawing.Size(100, 23);
+            this.Val2.TabIndex = 1;
+            // 
+            // Entrada
+            // 
+            this.Entrada.AutoSize = true;
+            this.Entrada.Location = new System.Drawing.Point(12, 77);
+            this.Entrada.Name = "Entrada";
+            this.Entrada.Size = new System.Drawing.Size(47, 15);
+            this.Entrada.TabIndex = 2;
+            this.Entrada.Text = "entrada";
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(12, 92);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(54, 15);
+            this.Resultado.TabIndex = 3;
+            this.Resultado.Text = "Processo";
+            // 
+            // Bt_iniciar
+            // 
+            this.Bt_iniciar.Location = new System.Drawing.Point(227, 11);
+            this.Bt_iniciar.Name = "Bt_iniciar";
+            this.Bt_iniciar.Size = new System.Drawing.Size(75, 23);
+            this.Bt_iniciar.TabIndex = 4;
+            this.Bt_iniciar.Text = "Iniciar";
+            this.Bt_iniciar.UseVisualStyleBackColor = true;
+            this.Bt_iniciar.Click += new System.EventHandler(this.Bt_iniciar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 127);
+            this.Controls.Add(this.Bt_iniciar);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.Entrada);
+            this.Controls.Add(this.Val2);
+            this.Controls.Add(this.Val1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Val1;
+        private System.Windows.Forms.TextBox Val2;
+        private System.Windows.Forms.Label Entrada;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.Button Bt_iniciar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaquinaDeTuring
+{
+    public partial class Form1 : Form
+    {
+
+        private char caratere = '*';
+        private int estado;
+
+        private string Resul;
+        private string proc;
+        string posicoes;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            clear();
+        }
+        //atualiza a visualização de cada modificação para o usuario
+        private void clear()
+        {
+            this.Entrada.Text = "-";
+            this.Resultado.Text = "-";
+            this.posicoes = "";
+            this.estado = 0;
+            this.Resul = "";
+            this.proc = "";
+        }
+        private void Bt_iniciar_Click(object sender, EventArgs e)
+        {
+            clear();
+            
+            int[] valores = new int[2];
+
+            //captura valores digitados pelo usuario
+            valores[0] = int.Parse(Val1.Text);
+            valores[1] = int.Parse(Val2.Text);
+
+            //pega o retorno da função listaPosiçôes
+            this.posicoes = listaPosicoes(valores);
+
+            //atribui o valor para Entrada, para que fique visivel para o usuario
+            Resul = posicoes;
+            Vsoma = this.posicoes.ToArray();
+            timer1.Start();
+            //soma();
+            //timer1.Stop();
+
+        }
+        /// <summary>
+        /// Gera uma string com caractere('*')*valor(n) + (' ')
+        /// </summary>
+        /// <param name="entrada"></param>
+        /// <returns></returns>
+        public string listaPosicoes(int[] entrada)
+        {
+            string saida = "";
+            foreach (var item in entrada)
+            {
+                for(int i =0; i< item; i++)
+                {
+                    saida += this.caratere;
+                }
+
+                saida += ' ';
+            }
+            return saida;
+        }
+
+        private string position(int posi, string valor)
+        {
+            char[] carac = valor.Replace(' ', '#').ToArray<char>();
+
+            carac[posi] = '+';
+            string t = SetText(carac);
+            //inserir o valor da posição varrida
+            return t.Replace("+", (posi+1).ToString());
+        }
+
+        private string SetText(char[] val_Result)
+        {
+            string result = "";
+            foreach (var item in val_Result)
+            {
+                result += item;
+            }
+            return result;
+        }
+        private void view()
+        {
+            if (!string.IsNullOrEmpty(Resul))
+            {
+                this.Entrada.Text = Resul;
+                this.Resultado.Text = proc;
+            }
+        }
+        int i = 0;
+        char[] Vsoma;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (this.estado == 0)
+            {
+                if (Vsoma[i] == this.caratere)
+                {
+                    this.estado = 0;
+                }
+                else
+                {
+                    this.estado = 1;
+                    Vsoma[i] = this.caratere;
+                }
+            }
+            else if (this.estado == 1)
+            {
+                if (Vsoma[i] == this.caratere)
+                {
+                    this.estado = 1;
+                }
+                else
+                {
+                    estado = 2;
+                }
+            }
+            else
+            {
+                i = i - 1;
+                Vsoma[i - 1] = ' ';
+                i = i - 1;
+                this.Resul = SetText(Vsoma);
+                this.proc = this.posicoes.Replace(this.caratere, ' ');
+                this.proc = position(i, proc);
+                view();
+                timer1.Stop();
+            }
+
+            this.Resul = SetText(Vsoma);
+            this.proc = this.posicoes.Replace(this.caratere, ' ');
+            this.proc = position(i, proc);
+            i+=1;
+            view();
+        }
+    }
+}
